722c0e640077709bd875a615d9bad2d029f54e78
DarkusRattus <kristian.benson@rutgers.edu>
2016-09-25 14:36:28 -0400

    Added Basket code

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class Basket : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Vector3 mousePos2D = Input.mousePosition; // Get current screen position of the mouse from input
+        mousePos2D.z = -Camera.main.transform.position.z; // The Camera's Z position sets how far to push the mouse into 3D
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); // Convert the point from 2D screen space into 3D game world space
+
+        // Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
 	}
+
+
 }
